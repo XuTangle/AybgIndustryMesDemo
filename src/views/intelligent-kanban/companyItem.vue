944da23495c7721fe9b42103c51706af4d644d56
刘玉娇 <18624321192@163.com>
2019-07-08 14:53:55 +0800

    新增页面代码 xjb

---
+++ src/views/intelligent-kanban/companyItem.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="item">
+    <div class="title">
+      <span>{{title}}</span>
+    </div>
+    <div class="mainTable">
+      <table>
+        <tr>
+          <td></td>
+          <td>日累计</td>
+          <td>月累计</td>
+        </tr>
+        <tr v-for="item in content" :key="item">
+          <td>{{item.head}}</td>
+          <td>{{item.dayValue}}吨</td>
+          <td>{{item.monthValue}}吨</td>
+        </tr>
+      </table>
+    </div>
+    <div class="mainFoot">
+      <div class="one">
+        <p class="number" id="adm" :style="typeColor">{{admission}}</p>
+        <p>入场</p>
+      </div>
+      <div class="one">
+        <p class="number test" :style="testColor">{{test}}</p>
+        <p>检验</p>
+      </div>
+      <div class="one">
+        <p class="number state" :style="stateColor">{{state}}</p>
+        <p>运行状态</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { truncate } from "fs";
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: Array,
+      required: true
+    },
+    admission: {
+      type: Number,
+      required: true
+    },
+    test: {
+      type: Number,
+      required: true
+    },
+    state: {
+      type: Number,
+      required: true
+    },
+  },
+  created() {
+    // debugger;
+    if (this.admission == 0) {
+      this.typeColor="background-color:yellow"
+    }
+  },
+  data() {
+    return {
+        typeColor:"background-color:aqua",
+        stateColor:"background-color:aqua",
+        testColor:"background-color:aqua",
+        content:null
+    };
+  }
+};
+</script>
+<style scoped>
+.item {
+  width: 30%;
+  height: 23vmin;
+  margin-left:2%;
+  margin-top: 2vmin;
+  margin-right: 1%;
+  border: solid 0.5px black;
+}
+.title {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+table td {
+  width: 13vmin;
+  font-size: 2vmin;
+}
+table {
+  margin: 1vmin 3vmin;
+}
+.one {
+  width: 9vmin;
+  font-size: 2vmin;
+  margin-left: 3vmin;
+}
+p {
+  padding: 0;
+  margin: 0;
+}
+.number {
+  width: 4vmin;
+  /* background-color: aqua; */
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+.mainFoot {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ src/views/intelligent-kanban/companyOverview.vue
@@ -0,0 +1,425 @@
+<template>
+  <div class="window">
+    <div class="aside">
+      <div class="asidTop">
+        <div class="clock"></div>
+        <span>安全生产天数：1066天</span>
+      </div>
+      <div class="asideMain">
+        <span>公司简介</span>
+        <div class="introduction" id="scroll-message">
+          <ul id="scroll1">
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+          </ul>
+        </div>
+      </div>
+      <div class="asidefoot">
+        <span>调度信息</span>
+        
+        <div class="information">
+          <table>
+          <th>
+            <td>监控项目</td>
+            <td>报警级别</td>
+            <td>所属区域</td>
+          </th>
+        </table>
+        <div id="scorllTale">
+           <ul>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+            <li>我们公司最棒！！！</li>
+          </ul>
+        </div>
+        </div>
+        <div class="log">
+          <span style="font-size:2vmin">调度日志</span>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+      <div class="title">
+        <span class="titleContent">XX钢厂全流程生产总览</span>
+      </div>
+      <div class="mainMain">
+        <div id="lineChart" class="mainTop"></div>
+        <div class="mainFoot">
+          <companyItem
+            v-for="key in dataList"
+            :key="key"
+            :title="key.title"
+            :content="key.content"
+            :admission="key.admission"
+            :test="key.test"
+            :state="key.state"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import echarts from "echarts";
+import companyItem from "./companyItem";
+// import { fetchData } from "@/api/Intelligence/overview";
+
+export default {
+  data() {
+    return {
+       dataList: [{
+          title: '原料厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        },
+        {
+          title: '烧结厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        },
+        {
+          title: '炼铁厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        },
+        {
+          title: '炼铁厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        },
+        {
+          title: '炼铁厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        },
+        {
+          title: '炼铁厂',
+          content: [{
+            head: '巴西矿',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'FMG',
+            dayValue: '3000',
+            monthValue: '15000'
+          },
+          {
+            head: 'PB',
+            dayValue: '3000',
+            monthValue: '15000'
+          }
+          ],
+          admission: 0,
+          test: 0,
+          state: 0
+        }
+        ]
+    };
+  },
+  created() {
+    //   this.scollor();
+    // fetchData().then(response => {
+    //   this.dataList = response.data.dataList;
+    // });
+  },
+  components: { companyItem },
+  mounted() {
+    this.initChart();
+    this.scoller("scroll-message");
+    this.scoller("scorllTale")
+  },
+  methods: {
+    scoller(byId){
+      setTimeout(function(){
+      var table = document.getElementById(byId);
+       var timer = null;
+       table.scrollTop = 0;
+       table.innerHTML += table.innerHTML;
+       function play() {
+           clearInterval(timer);
+           timer = setInterval(function() {
+               table.scrollTop++;
+               if (table.scrollTop >= table.scrollHeight / 2) {
+                   table.scrollTop = 0;
+               }
+           }, 100);
+       }
+       setTimeout(play, 500);
+       table.onmouseover = function() {
+           clearInterval(timer)
+       };
+
+       table.onmouseout = play;
+   },0)
+    },
+    initChart() {
+      this.chart = echarts.init(document.getElementById("lineChart"));
+
+      this.chart.setOption({
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          top: "10%",
+          containLabel: true
+        },
+        xAxis: {
+          type: "value",
+          show: false
+        },
+        yAxis: {
+          type: "category",
+          data: ["冷轧钢", "管线钢", "SPHC"],
+          splitLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLine: {
+            show: false
+          }
+        },
+        series: [
+          {
+            name: "直接访问",
+            type: "bar",
+            stack: "总量",
+            label: {
+              normal: {
+                show: true,
+                position: "insideRight"
+              }
+            },
+            data: [320, 302, 301]
+          },
+          {
+            name: "邮件营销",
+            type: "bar",
+            stack: "总量",
+            label: {
+              normal: {
+                show: true,
+                position: "insideRight"
+              }
+            },
+            data: [120, 132, 101],
+            barGap: 20,
+            barWidth: 25
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  /* border: solid 1px black */
+}
+.window {
+  display: flex;
+  flex-direction: row;
+}
+.aside {
+  width: 25.5%;
+  /* border: solid 1px black; */
+}
+.asidTop,
+.asideMain,.asidefoot {
+  width: 96%;
+  height: 33vmin;
+  margin-top: 1vmin;
+  margin-left: 1vmin;
+  border: solid 0.5px black;
+}
+.asidefoot{
+  height: 32vmin;
+}
+.asideMain {
+  height: 30vmin;
+}
+.clock {
+  height: 29vmin;
+}
+ul{
+  padding:0;margin:0;
+  padding-left: 2vmin
+}
+li{
+  padding:0;
+  margin:0;
+  font-size: 2vmin
+}
+.introduction {
+  margin: 1vmin;
+  height: 24vmin;
+  border: solid 0.5px black;
+  overflow:hidden;
+    /* float:right; */
+  /* padding-right:5%; */
+}
+.information{
+  height: 15vmin;
+  margin: 1vmin;
+  border: solid 0.5px black;
+}
+.log{
+  height: 11vmin;
+  margin: 1vmin;
+  border: solid 0.5px black;
+}
+.main {
+  width: 74%;
+  height: 99vmin;
+  /* border: solid 1px black; */
+}
+.title {
+  height: 13vmin;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+.titleContent {
+  font-size: 5vmin;
+}
+.mainMain {
+  height: 85vmin;
+  border: solid 0.5px black;
+}
+.mainTop {
+  height: 30vmin;
+}
+.mainFoot {
+  height: 56vmin;
+  display: flex;
+  flex-wrap: wrap;
+}
+#scorllTale{
+  height: 11.5vmin;
+  overflow:hidden;
+  /* border: solid 0.5px black; */
+}
+#scorllTale li{
+  list-style: none;
+}
+table{
+  width: 100%
+}
+td{
+  width: 42%;
+  text-align: left;
+  font-size: 1.5vmin
+}
+</style>
